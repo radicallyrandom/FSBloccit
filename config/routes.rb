2c621cfd9907a67c002285a5144386fea8a3c959
Brandon Clarke <weloveradicallyrandom@gmail.com>
2015-04-09 19:03:55 -0400

    Revert "Associate and nested topics and posts"
    This reverts commit aa59a301a1868ef47204d7c2609872990da5afc8.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-	
   devise_for :users
-
-	
-	resources :topics do 
-		resources :posts, except: [:index]
-	end
+	resources :posts
 
 	get 'about' => 'welcome#about'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,31 +12,16 @@ require 'faker'
 end
 users = User.all
 
-
-
-# Create Topics
-15.times do
-	Topic.create!(
-		name: Faker::Lorem.sentence,
-		description: Faker::Lorem.paragraph
-		)
-end
-topics = Topic.all
-
-
-
 # Create Posts
 50.times do 
 	Post.create!(
 		user: users.sample,
-		topic: topics.sample,
 		title: Faker::Lorem.sentence,
 		body:  Faker::Lorem.paragraph
 		)
 end
+
 posts = Post.all
-
-
 
 # Create Comments
 100.times do
@@ -47,56 +32,15 @@ posts = Post.all
 		)
 end
 
+user = User.first
+user.skip_reconfirmation!
+user.update_attributes!(
+   email: 'weloveradicallyrandom@gmail.com',
+   password: 'helloworld'
+ )
 
+ puts "Seed finished"
+ puts "#{User.count} users created"
+ puts "#{Post.count} posts created"
+ puts "#{Comment.count} comments created"
 
-#############################################
-#       START USER ROLE CREATIONS 			#
-#############################################
-
-
-# Create an admin user
-admin = User.new(
-	name: 			'Admin Meow',
-	email: 			'admin@example.com',
-	password: 		'helloworld',
-	role: 			'admin'
-)
-admin.skip_confirmation!
-admin.save!
-
-
-
-# Create a moderator
-moderator = User.new(
-	name: 			'Moderator Cuddles',
-	email: 			'moderator@example.com',
-	password: 		'helloworld',
-	role: 			'moderator'
-)
-moderator.skip_confirmation!
-moderator.save!
-
-
-
-# Create a member
-member = User.new(
-	name: 			'Member Peasant',
-	email: 			'member@example.com',
-	password: 		'helloworld',
-)
-member.skip_confirmation!
-member.save!
-
-
-#############################################
-#       END USER ROLE CREATIONS 			#
-#############################################
-
-
-
-# Verify Seeding in console
-puts "Seed finished"
-puts "#{User.count} users created"
-puts "#{Post.count} posts created"
-puts "#{Comment.count} comments created"
-puts "#{Topic.count} topics created"
