ce0ebefa3b3e7bea4518a59b4df996f64e2e2fce
Brandon Clarke <weloveradicallyrandom@gmail.com>
2015-04-09 23:03:16 -0400

    Complete post markdown assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,4 +10,21 @@ class Post < ActiveRecord::Base
   validates :user, presence: true
 
 	default_scope { order('created_at DESC') }
+
+	def markdown_title
+		render_as_markdown(self.title)
+	end
+
+	def markdown_body
+		render_as_markdown(self.body)
+	end
+
+	private
+
+	def render_as_markdown(text)
+		extensions = { fenced_code_blocks: true, disable_indented_code_blocks: true }
+		markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, extensions)
+		(markdown.render text).html_safe
+	end
+
 end
