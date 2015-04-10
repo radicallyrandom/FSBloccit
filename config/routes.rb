7a513a48172f8d95692016ec05691b6ecd0711e7
Brandon Clarke <weloveradicallyrandom@gmail.com>
2015-04-09 20:02:11 -0400

    Complete interlude assignment

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
 	
   devise_for :users
 
+        resources :users, only: [:update]
 	
 	resources :topics do 
 		resources :posts, except: [:index]
 	end
 
 	get 'about' => 'welcome#about'
-
   	root to: 'welcome#index'
 end
