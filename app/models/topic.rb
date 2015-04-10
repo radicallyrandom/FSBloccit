15ccdbf5640a2327fdc59fe805455ccd42938d44
Brandon Clarke <weloveradicallyrandom@gmail.com>
2015-04-09 21:55:06 -0400

    Complete validating post assignment

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,10 @@
 class Topic < ActiveRecord::Base
 	has_many :posts
+	
+	# Create a form partial for topics.
+	# Update #topics#edit and topics#new to use the topics#form partial.
+	# Use the form_group_tag helper method created in the checkpoint to display errors for invalid topics.
+
+	validates :name, length: { minimum: 5 }
+
 end
