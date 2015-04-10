ebfd136b6ac6c4e1c5753677f7c2a67ef4a3e254
Brandon Clarke <weloveradicallyrandom@gmail.com>
2015-04-09 21:13:08 -0400

    Validated Posts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,11 @@
 module ApplicationHelper
 
-	def my_name
-		"Brandon Clarke"
-	end	
+	def form_group_tag(errors, &block)
+		if errors.any?
+			content_tag :div, capture(&block), class: 'form-group has-error'
+		else
+			content_tag :div, capture(&block), class: 'form-group'
+		end	
+	end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,13 @@
 class Post < ActiveRecord::Base
-	has_many :comments
-	belongs_to :user
-	belongs_to :topic
+  
+  has_many :comments
+  belongs_to :user
+  belongs_to :topic
+
+  validates :title, length: { minimum: 5 }, presence: true
+  validates :body, length: { minimum: 20 }, presence: true
+  validates :topic, presence: true
+  validates :user, presence: true
 
 	default_scope { order('created_at DESC') }
 end
